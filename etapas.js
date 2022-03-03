4b47280c70fc6bedfe7869ebd2e2f94178e45d94
Willviana08 <wilkerman08@outlook.com>
2022-03-03 17:22:41 -0300

    Arquivos da Urna adicionados

---
+++ etapas.js
@@ -0,0 +1,66 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '10101',
+                nome: 'Goku Kakarotto',
+                partido: 'Sayadin',
+                fotos: [
+                    {url:'goku.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '20201',
+                nome: 'Vegeta',
+                partido: 'Sayadin',
+                fotos: [
+                    {url:'vegeta.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '30301',
+                nome: 'Naruto Uzumaki',
+                partido: 'Konoha',
+                fotos: [
+                    {url:'naruto.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '40401',
+                nome: 'Sasuke Uchiha',
+                partido: 'Konoha',
+                fotos: [
+                    {url:'sasuke.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '10',
+                nome: 'Goku Kakarotto',
+                partido: 'Sayadin',
+                vice: 'Vegeta',
+                fotos:[
+                    {url:'goku.jpg', legenda: 'Prefeito'},
+                    {url:'vegeta.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '20',
+                nome: 'Naruto Uzumaki',
+                partido: 'Konoha',
+                vice: 'Sasuke',
+                fotos:[
+                    {url:'naruto.jpg', legenda: 'Prefeito'},
+                    {url:'sasuke.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
